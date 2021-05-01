1c60bd40a32cfeab1d95e6f7aa4131688ceef374
syedmohin <786syedmohin@gmail.com>
2021-05-01 19:25:00 +0530

    Thrid Day

--- src/main/java/com/afc/utility/Alert.java
+++ src/main/java/com/afc/utility/Alert.java
@@ -1,27 +1,31 @@
 package com.afc.utility;
 
+import static com.jfoenix.controls.JFXButton.ButtonType.RAISED;
+import static javafx.scene.paint.Color.BLUE;
+import static javafx.scene.paint.Color.RED;
+import static javafx.stage.Modality.APPLICATION_MODAL;
+
 import com.jfoenix.controls.JFXAlert;
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXButton.ButtonType;
 import com.jfoenix.controls.JFXDialogLayout;
 
 import javafx.scene.control.Label;
 import javafx.scene.effect.BoxBlur;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import javafx.stage.Modality;
 
 public class Alert {
 	private Alert() {
 	}
 
+	private static final String OKAY = "Okay!";
+
 	public static void error(String msg, Pane root) {
 		var alert = alert(root);
 		var layout = layout(msg, "Error");
-		var btn = button(alert, "Okay!");
-		btn.setRipplerFill(Color.RED);
+		var btn = button(alert, OKAY);
+		btn.setRipplerFill(RED);
 		layout.setActions(btn);
 		alert.setContent(layout);
 		alert.showAndWait();
@@ -30,8 +34,8 @@ public class Alert {
 	public static void info(String msg, Pane root) {
 		var alert = alert(root);
 		var layout = layout(msg, "Info");
-		var btn = button(alert, "Okay!");
-		btn.setRipplerFill(Color.BLUE);
+		var btn = button(alert, OKAY);
+		btn.setRipplerFill(BLUE);
 		layout.setActions(btn);
 		alert.setContent(layout);
 		alert.showAndWait();
@@ -40,8 +44,8 @@ public class Alert {
 	public static JFXButton button(JFXAlert<String> alert, String msg) {
 		var btn = new JFXButton(msg);
 		btn.setFont(new Font(15));
-		btn.setButtonType(ButtonType.RAISED);
-		btn.setRipplerFill(Color.RED);
+		btn.setButtonType(RAISED);
+		btn.setRipplerFill(RED);
 		btn.setCancelButton(true);
 		btn.setOnAction(e -> alert.hideWithAnimation());
 		return btn;
@@ -49,7 +53,7 @@ public class Alert {
 
 	public static JFXAlert<String> alert(Pane root) {
 		var alert = new JFXAlert<String>(root.getScene().getWindow());
-		alert.initModality(Modality.APPLICATION_MODAL);
+		alert.initModality(APPLICATION_MODAL);
 		alert.setOverlayClose(false);
 		root.setEffect(new BoxBlur(3, 3, 3));
 		alert.setOnCloseRequest(ae -> root.setEffect(null));
